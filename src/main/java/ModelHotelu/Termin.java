62d144c0b2d0402e74bf10f831fa1e293ad0c8d0
hjxxzk <273017@student.pwr.edu.pl>
2025-01-04 17:08:37 +0100

    Pokoje Arraylist reading from json file

--- src/main/java/ModelHotelu/Termin.java
+++ src/main/java/ModelHotelu/Termin.java
@@ -1,25 +1,36 @@
 package ModelHotelu;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalDate;
 
 public class Termin {
 
-	private LocalDate data_rozpoczecia_pobytu;
-	private LocalDate data_zakonczenia_pobytu;
+	private String data_rozpoczecia_pobytu;
+	private String data_zakonczenia_pobytu;
 
-	public LocalDate getData_rozpoczecia_pobytu() {
+	@JsonCreator
+	public Termin(
+			@JsonProperty("od") String data_rozpoczecia_pobytu,
+			@JsonProperty("do") String data_zakonczenia_pobytu) {
+		this.data_rozpoczecia_pobytu = data_rozpoczecia_pobytu;
+		this.data_zakonczenia_pobytu = data_zakonczenia_pobytu;
+	}
+
+	public String getData_rozpoczecia_pobytu() {
 		return this.data_rozpoczecia_pobytu;
 	}
 
-	public void setData_rozpoczecia_pobytu(LocalDate data_rozpoczecia_pobytu) {
+	public void setData_rozpoczecia_pobytu(String data_rozpoczecia_pobytu) {
 		this.data_rozpoczecia_pobytu = data_rozpoczecia_pobytu;
 	}
 
-	public LocalDate getData_zakonczenia_pobytu() {
+	public String getData_zakonczenia_pobytu() {
 		return this.data_zakonczenia_pobytu;
 	}
 
-	public void setData_zakonczenia_pobytu(LocalDate data_zakonczenia_pobytu) {
+	public void setData_zakonczenia_pobytu(String data_zakonczenia_pobytu) {
 		this.data_zakonczenia_pobytu = data_zakonczenia_pobytu;
 	}
 
--- src/main/java/PrezenterHotelu/Prezenter.java
+++ src/main/java/PrezenterHotelu/Prezenter.java
@@ -1,20 +1,59 @@
 package PrezenterHotelu;
 
+import ModelHotelu.Pokoj;
+import ModelHotelu.Termin;
 import ViewHotelu.IMenuView;
 import ViewHotelu.RecepcjonistkaMenuView;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Prezenter {
 
 	public static void main(String[] args) {
-		IMenuView menuRecepcjonistki = new RecepcjonistkaMenuView();
-		menuRecepcjonistki.wyswietl();
+		setMenu();
+		//IMenuView menuRecepcjonistki = new RecepcjonistkaMenuView();
+		//menuRecepcjonistki.wyswietl();
 
 	}
 
 
-	public void setMenu() {
-		// TODO - implement Prezenter.setMenu
-		throw new UnsupportedOperationException();
+	public static void setMenu() {
+		File plikJSON = new File("---sciezka-do-pliku---");
+
+		// Inicjalizacja ObjectMapper (do parsowania JSON)
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		try {
+			// Zmienia zawartość pliku JSON na List<Pokoj>
+			List<Pokoj> pokoje = objectMapper.readValue(plikJSON, new TypeReference<List<Pokoj>>(){});
+
+			// Iterowanie po pokoju i wyświetlanie danych
+			for (Pokoj pokoj : pokoje) {
+				System.out.println("Numer pokoju: " + pokoj.getNumer());
+				System.out.println("Pietro: " + pokoj.getPietro());
+				System.out.println("Liczba gosci: " + pokoj.getLiczbaGosci());
+				System.out.println("Standard: " + pokoj.getStandard());
+				System.out.println("Udogodnienia: " + pokoj.getUdogodnienia());
+				System.out.println("Koszt: " + pokoj.getKoszt());
+				System.out.println("Terminy:");
+				for (Termin termin : pokoj.getTerminy()) {
+					System.out.println("Od: " + termin.getData_rozpoczecia_pobytu() + " Do: " + termin.getData_zakonczenia_pobytu());
+				}
+				System.out.println("-----------------------");
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
