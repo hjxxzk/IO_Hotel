7993ba2d0e13f243c1b2d73887339c3312e71354
8RiVeR8 <maks.rzeczycki@gmail.com>
2025-01-09 14:03:03 +0100

    add test for reservation class

--- src/main/java/ViewHotelu/RecepcjonistkaMenuView.java
+++ src/main/java/ViewHotelu/RecepcjonistkaMenuView.java
@@ -26,6 +26,7 @@ public class RecepcjonistkaMenuView implements IMenuView {
 
 	Logowanie logowanie = new Logowanie();
 	HotelFasada hotel;
+	//RezerwacjaView rezerwacja = new RezerwacjaView();
 	PokojView pokoje = new PokojView();
 	JList<String> guestList;
 	RezerwacjaView rezerwacja = new RezerwacjaView(hotel);
--- src/test/java/ModelHotelu/RezerwacjaTest.java
+++ src/test/java/ModelHotelu/RezerwacjaTest.java
@@ -1,14 +1,60 @@
 package ModelHotelu;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RezerwacjaTest {
+    HotelFasada hotel;
+    @BeforeEach
+    void getData() {
+        hotel = new HotelFasada();
+        File plikPokojeJSON = new File("/Users/maks_rz/Desktop/Studia/Semestr 5/Inżynieria oprogramowania/HotelProject/src/main/resources/pokoje.json");
+        File rezerwacjePokojeJSON = new File("/Users/maks_rz/Desktop/Studia/Semestr 5/Inżynieria oprogramowania/HotelProject/src/main/resources/rezerwacje.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            List<Pokoj> pokoje = objectMapper.readValue(plikPokojeJSON, new TypeReference<List<Pokoj>>(){});
+            List<Rezerwacja> rezerwacje = objectMapper.readValue(rezerwacjePokojeJSON, new TypeReference<List<Rezerwacja>>() {});
+
+            hotel.setPokoje(pokoje);
+            hotel.setRezerwacje(rezerwacje);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
-    void generateReservationNumber() {
+    void shouldGenerateReservationNumberWithCurrentDate() {
+        Rezerwacja rezerwacja = new Rezerwacja(2, 0, null, "12:40", null, null, null);
+        String dataNumeruRezerwacji = rezerwacja.getNumerRezerwacji().substring(0, 10);
+        String numerRezerwacji = rezerwacja.getNumerRezerwacji().substring(12, 16);
+        assertEquals(rezerwacja.getCurrentDate(), dataNumeruRezerwacji);
+        assertTrue(Integer.parseInt(numerRezerwacji) > 0);
+    }
 
+    @ParameterizedTest
+    @CsvSource(value = {
+            "5, 2, null, 12:34, null, null, null",
+            "3, 7, null, 10:11, null, null, null",
+            "1, 0, null, 15:52, null, null, null"
+    }, nullValues = {"null"})
+    void shouldGenerateReservationNumberWithCurrentDateForDifferentReservations(int iloscDoroslych, int iloscDzieci, Platnosc platnosc, String godzinaPrzyjazdu, Gosc gosc, Pokoj pokoj, Termin termin) {
+        Rezerwacja rezerwacja = new Rezerwacja(iloscDoroslych, iloscDzieci, platnosc, godzinaPrzyjazdu, gosc, pokoj, termin);
+        String dataNumeruRezerwacji = rezerwacja.getNumerRezerwacji().substring(0, 10);
+        String numerRezerwacji = rezerwacja.getNumerRezerwacji().substring(12, 16);
+        assertEquals(rezerwacja.getCurrentDate(), dataNumeruRezerwacji);
+        assertTrue(Integer.parseInt(numerRezerwacji) > 0);
     }
 
     @Test
