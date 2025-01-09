775f749e723354f9308980657fdd53cf52ac3560
8RiVeR8 <maks.rzeczycki@gmail.com>
2025-01-09 14:59:08 +0100

    change CsVSource to MethodSource in reservation number test

--- src/test/java/ModelHotelu/RezerwacjaTest.java
+++ src/test/java/ModelHotelu/RezerwacjaTest.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -44,11 +47,7 @@ class RezerwacjaTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {
-            "5, 2, null, 12:34, null, null, null",
-            "3, 7, null, 10:11, null, null, null",
-            "1, 0, null, 15:52, null, null, null"
-    }, nullValues = {"null"})
+    @MethodSource("dataForCreatingReservations")
     @Order(3)
     void shouldGenerateReservationNumberWithCurrentDateForDifferentReservations(int iloscDoroslych, int iloscDzieci, Platnosc platnosc, String godzinaPrzyjazdu, Gosc gosc, Pokoj pokoj, Termin termin) {
         Rezerwacja rezerwacja = new Rezerwacja(iloscDoroslych, iloscDzieci, platnosc, godzinaPrzyjazdu, gosc, pokoj, termin);
@@ -66,5 +65,13 @@ class RezerwacjaTest {
             rezerwacja.checkIn();
             assertTrue(rezerwacja.isZameldowanie());
         }
+    }
+
+    static Stream<Arguments> dataForCreatingReservations() {
+        return Stream.of(
+                Arguments.of(5, 2, null, "12:34", null, null, null),
+                Arguments.of(3, 7, null, "10:11", null, null, null),
+                Arguments.of(1, 0, null, "15:52", null, null, null)
+        );
     }
 }
