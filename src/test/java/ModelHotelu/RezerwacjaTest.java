2ba534e84a365ed111ab7c8161f9f1161c7896dd
hjxxzk <273017@student.pwr.edu.pl>
2025-01-09 11:29:31 +0100

    add more working tests

---
+++ src/test/java/ModelHotelu/RezerwacjaTest.java
@@ -0,0 +1,18 @@
+package ModelHotelu;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RezerwacjaTest {
+
+    @Test
+    void generateReservationNumber() {
+
+    }
+
+    @Test
+    void checkIn() {
+
+    }
+}
---
+++ src/test/java/ModelHotelu/TagTest.java
@@ -0,0 +1,4 @@
+package ModelHotelu;
+
+public class TagTest {
+}
