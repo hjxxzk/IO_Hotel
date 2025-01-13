9df06d4a7d04aed7ea268fa2a87acc515d6f512e
8RiVeR8 <maks.rzeczycki@gmail.com>
2025-01-13 22:02:59 +0100

    add @Tag to tests in PokojTest

--- src/test/java/ModelHotelu/PokojTest.java
+++ src/test/java/ModelHotelu/PokojTest.java
@@ -2,16 +2,21 @@ package ModelHotelu;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(CustomExceptionHandler.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class PokojTest {
     HotelFasada hotel;
     @BeforeEach
@@ -34,6 +39,8 @@ class PokojTest {
     }
 
     @Test
+    @Order(1)
+    @Tag("calculation")
     void shouldReturn750ForRoom101_whenGuestStaysFor3Days() {
         List<Pokoj> pokoje = hotel.getPokoje();
         String dataRozpoczecia = "2025-01-07";
@@ -54,6 +61,8 @@ class PokojTest {
             "102, 2025-02-01, 2025-02-05, 600",
             "103, 2025-03-10, 2025-03-15, 1250"
     })
+    @Order(2)
+    @Tag("calculation")
     void shouldReturnExpectedPriceForRoom_whenGuestStaysForSpecificDays(String wybranyPokoj, String dataRozpoczecia, String dataZakonczenia, int expectedPrice) {
         List<Pokoj> pokoje = hotel.getPokoje();
         for (Pokoj pokoj : pokoje) {
@@ -65,6 +74,8 @@ class PokojTest {
     }
 
     @Test
+    @Order(3)
+    @Tag("reservation")
     void shouldAddGivenReservationDate() {
         String wybranyPokoj = "101";
         String dataRozpoczecia = "2025-01-14";
@@ -78,6 +89,32 @@ class PokojTest {
                 assertTrue(listaTerminowDlaPokoju.contains(termin));
             }
         }
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideReservationData")
+    @Order(4)
+    @Tag("reservation")
+    void shouldAddGivenReservationDate(String wybranyPokoj, String dataRozpoczecia, String dataZakonczenia) {
+        Termin termin = new Termin(dataRozpoczecia, dataZakonczenia);
+        List<Pokoj> pokoje = hotel.getPokoje();
+
+        for (Pokoj pokoj : pokoje) {
+            if (pokoj.getNumer().equals(wybranyPokoj)) {
+                pokoj.addReservationDate(termin);
+                List<Termin> listaTerminowDlaPokoju = pokoj.getTerminy();
+                assertTrue(listaTerminowDlaPokoju.contains(termin),
+                        "Lista terminów powinna zawierać dodany termin: " + termin);
+            }
+        }
+    }
+
+    static Stream<Arguments> provideReservationData() {
+        return Stream.of(
+                Arguments.of("101", "2025-01-14", "2025-01-15"),
+                Arguments.of("102", "2025-02-01", "2025-02-05"),
+                Arguments.of("103", "2025-03-10", "2025-03-15")
+        );
     }
 
 
